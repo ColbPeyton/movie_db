3b3fc90cd327b8c28eae04f322247d30870a7d34
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-10-19 16:36:14 -0500

    add placeholder app routes for sorting and movie details

--- app/backend/app.rb
+++ app/backend/app.rb
@@ -2,5 +2,15 @@ require 'sinatra'
 
 # http://localhost:4567/list/complete
 get '/list/complete' do
-    "complete list of movies"
-  end
+  "complete list of movies"
+end
+
+# matches "GET /list/sort?direction=up"
+get '/list/sort' do
+  direction = params['direction']
+end
+
+# matches "GET /list/title?movie_id=12"
+get '/list/title' do
+  movie_id = params['movie_id']
+end
