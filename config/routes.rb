92f87b8ce401c381762b9c3304c1e6e3f29683dd
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-11-28 10:19:42 -0600

    update markup add basic styling. update routes to include start screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root  'start_screen#index'
+  root 'start_screen#index', as: 'home'
+  get  '/home', to: 'start_screen#index'
   get '/movie', to: 'movie#index', as: 'movie'
   get '/search', to: 'movie_collection#index', as: 'search'
 end
